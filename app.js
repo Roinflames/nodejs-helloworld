79a32c2f1a7a12dcd0c22a969d2986ec82509fab
EC3334 <rodrigo.reyessilva@externos-cl.cencosud.com>
2021-11-08 12:52:38 -0300

    borro hostname que puede estar causando error

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const http = require("http");
-
-const hostname = "127.0.0.1";
-const port = process.env.PORT || 3000;
+// const hostname = "127.0.0.1";
+const port = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -9,6 +8,6 @@ const server = http.createServer((req, res) => {
   res.end("Hola Mundo");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`El servidor TECSOS-TEST se está ejecutando en ${port}`);
 });
